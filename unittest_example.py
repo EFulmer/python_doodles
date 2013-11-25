598c464f5faab412342e887428a558881589fb97
Eric Fulmer <EFulmer@gmail.com>
2013-11-24 19:15:24 -0500

    unit test example

---
+++ unittest_example.py
@@ -0,0 +1,31 @@
+# From http://www.oreillynet.com/lpt/a/5463
+
+import unittest
+
+
+# Here's our "unit".
+def IsOdd(n):
+        return n % 2 == 1
+
+
+# Here's our "unit tests".
+class IsOddTests(unittest.TestCase):
+    
+    # Eric's notes:
+    # all "test*" methods are test suites.
+    # guess: all "test*"s run when unittest.main() is called?
+    # if implemented, setUp called before each method and tearDown
+    # after each.
+    def testOne(self):
+        self.failUnless(IsOdd(1))
+    
+    def testTwo(self):
+        self.failIf(IsOdd(2))
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
