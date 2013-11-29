4b127eafc7683762c0bc0d9d985b6441d4825292
Eric Fulmer <EFulmer@gmail.com>
2013-11-29 15:40:22 -0500

    fizzbuzz without the dreaded 15!

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -9,9 +9,21 @@ def fizzbuzz():
         else:
             print(i)
 
+def fizz_buzz():
+    for i in range(1, 101):
+        if not (i % 3 == 0 or i % 5 == 0):
+            print(i)
+        else:
+            if i % 3 == 0:
+                print('Fizz', end='')
+            if i % 5 == 0:
+                print('Buzz', end='')
+            print('')
+
 
 def main():
     fizzbuzz()
+    fizz_buzz()
 
 
 if __name__ == '__main__':
