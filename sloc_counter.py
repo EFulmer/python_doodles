85aec0168610286bdb7560f44971ccec12d5f58f
Eric Fulmer <EFulmer@gmail.com>
2013-12-02 20:52:34 -0500

    updated sloc_counter to handle whitespace

--- sloc_counter.py
+++ sloc_counter.py
@@ -1,7 +1,18 @@
 import re
+import sys
 
 def python_sloc(src, count_whitespace=True):
-    python_comments = re.compile('\s*#')
+    """
+    Returns number of non-comment lines of code in src. 
+    If whitespace=False, whitespace lines are not counted either.
+    """
+
+    if count_whitespace:
+        python_comments = re.compile('\s*#')
+    else:
+        python_comments = re.compile('^\s*$')
+
     with open(src, 'r') as f:
         lines = f.readlines()
         return sum([1 for l in lines if not re.match(python_comments, l)])
+
